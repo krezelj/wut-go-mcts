cd7de5eec09890ea5b3c508b637b17c67a5fddf3
Jasiek <jasiekk9@gmail.com>
2024-05-06 18:22:43 +0200

    Dodaj pliki projektów.

---
+++ wut-go-mcts/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace wut_go_mcts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
