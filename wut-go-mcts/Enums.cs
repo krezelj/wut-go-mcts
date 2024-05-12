88ba9b9176c3d53635bf7913ed3df9574634d3b1
Jasiek <jasiekk9@gmail.com>
2024-05-12 19:29:12 +0200

    Add flags enum

--- wut-go-mcts/Enums.cs
+++ wut-go-mcts/Enums.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace wut_go_mcts
+﻿namespace wut_go_mcts
 {
     public enum Direction { Left = 1, Right = -1, Up = 10, Down = -10, 
                             LeftUp = 11, LeftDown = -9, RightUp = 9, RightDown = -11};
+
+    public enum Flags { SideToPlay = 0b000, Pass = 0b010, Finished = 0b100}
 }
