c228631107c8fa571877eec344ea01dbd60c8052
Jasiek <jasiekk9@gmail.com>
2024-05-12 19:29:33 +0200

    Add move struct

---
+++ wut-go-mcts/Move.cs
@@ -0,0 +1,26 @@
+﻿namespace wut_go_mcts
+{
+    public struct Move
+    {
+        public Bitboard Position;
+        public Bitboard Captures;
+        public bool IsPass => Position.IsEmpty();
+
+        public Move()
+        {
+            Position = new Bitboard();
+            Captures = new Bitboard();
+        }
+
+        public Move(Bitboard position, Bitboard captures)
+        {
+            Position = position;
+            Captures = captures;
+        }
+
+        public static Move Pass()
+        {
+            return new Move(new Bitboard(), new Bitboard());
+        }
+    }
+}
